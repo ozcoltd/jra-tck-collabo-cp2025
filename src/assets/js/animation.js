6d47b373e2ad39ee88b5c44b545c4eff88bf3f3c
本間麻季 <maki.honma@oz-design.jp>
2025-05-09 12:55:23 +0900

    chore: 調整

--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -1,35 +1,52 @@
 export function Animation() {
     const elm = document.querySelector('.js-animation');
-    const ua = navigator.userAgent;
-    if (/iPhone|Android|Mobile|iPad/.test(ua)){
-        elm.remove();
-        return;
-    }
 
-    const fvFlg = sessionStorage.getItem('FirstView');
-    if(fvFlg){
-        elm.remove();
-        return;
-    }
-
-    if(elm) {
-        elm.classList.remove('is-hidden');
-    }
+    // const fvFlg = sessionStorage.getItem('FirstView');
+    // if(fvFlg){
+    //     elm.remove();
+    //     return;
+    // }
 
     const delay = 2000;
     let timeoutTask;
+    const ua = navigator.userAgent;
+    const uaData = navigator.userAgentData;
+    let devSp = false;
+
+    //PCのみ、ファーストビュー発生
+    if (uaData && uaData.brands.length > 0 && uaData.platform === 'iPad') {
+        devSp = uaData.mobile;
+    } else {
+        devSp = /iPhone|Android|Mobile|iPad/.test(ua);
+    }
+    if(devSp) {
+        if(elm) elm.remove();
+        return;
+    } else {
+        if(elm) elm.classList.remove('is-hidden');
+    }
+
+    //ユーザスクロール操作、2秒経過でファーストビューがフェードアウト
+    window.addEventListener('load', () => {
+        requestAnimationFrame(() => {
+            const sy = window.scrollY;
     
-    const handleScroll = () => {
-        elm.classList.add('is-end');
-        sessionStorage.setItem('FirstView', 'true');
-        clearTimeout(timeoutTask);
-        window.removeEventListener('scroll', handleScroll);
-    };
-
-    timeoutTask = setTimeout(() => {
-        elm.classList.add('is-end');
-        sessionStorage.setItem('FirstView', scrollY);
-    }, delay);
-
-    window.addEventListener('scroll', handleScroll);
+            const handleScroll = () => {
+                if (window.scrollY !== sy) {
+                    elm.classList.add('is-end');
+                    clearTimeout(timeoutTask);
+                    window.removeEventListener('scroll', handleScroll);
+                    // sessionStorage.setItem('FirstView', 'true');
+                }
+            };
+    
+            timeoutTask = setTimeout(() => {
+                elm.classList.add('is-end');
+                // sessionStorage.setItem('FirstView', 'true');
+            }, delay);
+    
+            window.addEventListener('scroll', handleScroll);
+        });
+    });
+    
 }
